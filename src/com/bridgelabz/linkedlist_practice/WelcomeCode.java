2949e344c736bb138a62b3b6487f23819acd7f5c
Sam-0517 <samadhankorde2000@gmail.com>
2023-01-28 08:06:21 +0530

    Added: Welcome message to linkedlist implementation program

---
+++ src/com/bridgelabz/linkedlist_practice/WelcomeCode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist_practice;
+
+public class WelcomeCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO LINKEDLIST IMPLEMENTATION PROGRAM...");
+	}
+
+}
